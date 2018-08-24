8eb258a96d4528fbbe0ac33a4d4c6d8400c3698c
Jakob Löw <jakob@m4gnus.de>
2018-08-25 01:58:20 +0200

    put all files in a src/ subdirectory and change Add/Sub to use an array of summands/factors

--- src/Solver.hs
+++ src/Solver.hs
@@ -1,26 +1,36 @@
+module Solver where
+
 import Data.Ratio
+import Data.List
 
 data Constant = Pi | E
     deriving(Show, Eq)
-data UnaryOperator = Minus | Factorial | Sin | Cos | Tan
+data UnaryOperator = Minus | Div | Factorial | Sin | Cos | Tan
     deriving(Show, Eq)
-data BinaryOperator = Add | Sub | Mul | Div | Mod | Exp | Log
+data BinaryOperator = Mod | Exp | Log
+    deriving(Show, Eq)
+data MultiOperator = Add | Mul
     deriving(Show, Eq)
 data Expression = Value Rational
                 | Variable String
                 | Constant Constant
                 | Unary UnaryOperator Expression
                 | Binary BinaryOperator Expression Expression
+                | Multi MultiOperator [Expression]
     deriving(Eq)
 data Equation = Equation Expression Expression
+    deriving(Eq)
 
 instance Show Expression where
     show (Value val)                 = show $ fromRational val
     show (Variable name)             = name
     show (Constant Pi)               = "π"
     show (Constant E)                = "e"
-    show (Unary Factorial expr)      = (show expr) ++ "!"
     show (Unary Minus expr)          = '-' : (show expr)
+    show (Unary Factorial expr)      = (show expr) ++ "!"
     show (Unary op expr)             = (show op) ++ "(" ++ (show expr) ++ ")"
     --show (Binary Exp a (Value val))  = --TODO pretty print using "¹²³⁴⁵⁶⁷⁸⁹"
     show (Binary op a b)             = "(" ++ (show a) ++ " " ++ (show op) ++ " " ++ (show b) ++ ")"
+    show (Multi Add exprs)           = "(" ++ (intercalate " + " $ map show exprs) ++ ")"
+    --TODO pretty print Div's in Mul
+    show (Multi Mul exprs)           = "(" ++ (intercalate " * " $ map show exprs) ++ ")"
