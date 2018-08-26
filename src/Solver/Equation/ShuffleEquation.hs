7245c7f38211bef8d96828ca6055850932783a71
Christian Schwarz <schwarz.christian.de@gmail.com>
2018-08-26 11:58:29 +0200

    change neutral element of multiplication to one, improve spacing etc, add TODO

--- src/Solver/Equation/ShuffleEquation.hs
+++ src/Solver/Equation/ShuffleEquation.hs
@@ -2,34 +2,12 @@ module Solver.Equation.ShuffleEquation where
 import Solver
 import Data.List
 
-shuffleMultiAdd :: [Expression] -> [Expression] -> [Equation]
-shuffleMultiAdd left right = 
-    map (\x -> case x of
-            ([], r) -> (Equation (Value 0) (Multi Add r))
-            (l, []) -> (Equation (Multi Add l) (Value 0))
-            ([v], r) -> (Equation v (Multi Add r))
-            (l, [v]) -> (Equation (Multi Add l) v)
-            (l, r) -> (Equation (Multi Add l) (Multi Add r))
-        )
-        (separations left right (\s -> case s of
-                            Unary Minus f -> f
-                            f -> Unary Minus f
-                         )
-        )
-shuffleMultiMul :: [Expression] -> [Expression] -> [Equation]
-shuffleMultiMul left right = 
-    map (\x -> case x of
-            ([], r) -> (Equation (Value 0) (Multi Mul r))
-            (l, []) -> (Equation (Multi Mul l) (Value 0))
-            ([v], r) -> (Equation v (Multi Mul r))
-            (l, [v]) -> (Equation (Multi Mul l) v)
-            (l, r) -> (Equation (Multi Mul l) (Multi Mul r))
-        )
-        (separations left right (\s -> case s of
-                            Unary Div f -> f
-                            f -> Unary Div f
-                         )
-        )
+--TODO: think about problems like:
+--dividing by x
+--squaring creates false solutions
+--switching comparator sign in inequations
+--roots creating plus minus 
+--saving applied moves
 shuffleEquation:: Equation -> [Equation]
 shuffleEquation equation =
     case equation of 
@@ -42,8 +20,36 @@ shuffleEquation equation =
                 (Multi Mul a, b) -> shuffleMultiMul a [b]
                 (a, Multi Mul b) -> shuffleMultiMul [a] b
                    
-                
-                        
+shuffleMultiAdd :: [Expression] -> [Expression] -> [Equation]
+shuffleMultiAdd left right = 
+    map (\x -> case x of
+            ([], r) -> (Equation (Value 0) (Multi Add r))
+            (l, []) -> (Equation (Multi Add l) (Value 0))
+            ([v], r) -> (Equation v (Multi Add r))
+            (l, [v]) -> (Equation (Multi Add l) v)
+            (l, r) -> (Equation (Multi Add l) (Multi Add r))
+        )
+        (separations left right (\s -> case s of
+                                    Unary Minus f -> f
+                                    f -> Unary Minus f
+                                )
+        )
+
+shuffleMultiMul :: [Expression] -> [Expression] -> [Equation]
+shuffleMultiMul left right = 
+    map (\x -> case x of
+            ([], r) -> (Equation (Value 1) (Multi Mul r))
+            (l, []) -> (Equation (Multi Mul l) (Value 1))
+            ([v], r) -> (Equation v (Multi Mul r))
+            (l, [v]) -> (Equation (Multi Mul l) v)
+            (l, r) -> (Equation (Multi Mul l) (Multi Mul r))
+        )
+        (separations left right (\s -> case s of
+                                    Unary Div f -> f
+                                    f -> Unary Div f
+                                )
+        )                
+
 --this could be greatly optimized, e.g.  
 --by taking the remaining list length as parameter to stop recursion when (len list) < m
 --but we will obey Knuth in this case
@@ -60,6 +66,3 @@ separations a b switcher =
         separate_n remaining m left right = 
             (separate_n (tail remaining) (m-1) (left ++ [switcher $ head remaining]) right) ++
             (separate_n (tail remaining) m left (right ++ [head remaining]))
-
-                
-
