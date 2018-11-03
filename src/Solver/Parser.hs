9ae9c6b3c7b045df5493062918743a4ce8bcd0f1
Christian Schwarz <schwarz.christian.de@gmail.com>
2018-11-03 22:25:37 +0100

    fix parser operator precedence, fix indentation in Parser.hs

--- src/Solver/Parser.hs
+++ src/Solver/Parser.hs
@@ -15,6 +15,7 @@ data Token = TUnaryOp Char
            | TParenClose 
     deriving(Show)
 
+
 tokenize :: String -> Bool -> [Token] 
 tokenize (s:xs) prev_op 
     | isSpace s                             = tokenize xs prev_op
@@ -33,61 +34,51 @@ tokenize [] prev_op = []
 keepWhile :: (a -> Bool) -> [a] -> ([a], [a]) 
 keepWhile f l = (\a b -> (a, drop (length a) b)) (takeWhile f l) l 
 
+isLeftAssociative :: Token -> Bool
+isLeftAssociative (TBinaryOp '^')    = False
+isLeftAssociative _                  = True
+
 prec :: Token -> Int
 prec (TUnaryOp o)
-    | o == '-' = 10
-    | o == '+' = 10
+    | o == '-'  = 10
+    | o == '+'  = 10
 prec (TBinaryOp o)
-    | o == '-' = 1
-    | o == '+' = 1
-    | o == '*' = 2
-    | o == '/' = 2
-    | o == '^' = 4
+    | o == '-'  = 1
+    | o == '+'  = 1
+    | o == '*'  = 2
+    | o == '/'  = 2
+    | o == '^'  = 4
 prec TParenOpen = 0
-
 
 parseExpression :: String -> Expression
 parseExpression s = shuntingYard (tokenize s True) [] []
         
+shuntingYardOp (t:ts) ops expr = shuntingYard ts (t:ops') expr'
+    where
+        (ops', expr') = collapseWhile shouldCollapse ops expr
+        shouldCollapse o = (prec o > prec t) || (prec o == prec t && isLeftAssociative o) 
+
 shuntingYard :: [Token] -> [Token] -> [Expression] -> Expression
-
-shuntingYard ((TValue v):ts) ops expr = 
-    shuntingYard ts ops (Value v : expr)
-
-shuntingYard ((TVariable v):ts) ops expr = 
-    shuntingYard ts ops (Variable v : expr)
-
-shuntingYard (t@(TUnaryOp u):ts) ops expr = shuntingYard ts (t:ops') expr'
+shuntingYard ((TValue v):ts) ops expr       = shuntingYard ts ops (Value v : expr)
+shuntingYard ((TVariable v):ts) ops expr    = shuntingYard ts ops (Variable v : expr)
+shuntingYard ts@((TUnaryOp u):_) ops expr   = shuntingYardOp ts ops expr
+shuntingYard ts@((TBinaryOp b):_) ops expr  = shuntingYardOp ts ops expr
+shuntingYard (TParenOpen : ts) ops expr     = shuntingYard ts (TParenOpen : ops) expr
+shuntingYard (TParenClose : ts) ops expr    = shuntingYard ts ops' expr'
     where
-        (ops', expr') = collapseWhile ((>(prec t)) . prec) ops expr
-
-shuntingYard (t@(TBinaryOp b):ts) ops expr = shuntingYard ts (t:ops') expr'
+        ((_: ops'), expr') = collapseWhile (not . isParenOpen) ops expr
+            where 
+                isParenOpen TParenOpen = True
+                isParenOpen _          = False
+shuntingYard [] ops expr                    = result 
     where
-        (ops', expr') = collapseWhile ((>(prec t)) . prec) ops expr
-
-shuntingYard (TParenOpen : ts) ops expr =
-    shuntingYard ts (TParenOpen : ops) expr
-
-shuntingYard (TParenClose : ts) ops expr =
-    shuntingYard ts ops' expr'
-        where
-            ((_: ops'), expr') = collapseWhile (not . isParenOpen) ops expr
-                where 
-                    isParenOpen TParenOpen = True
-                    isParenOpen _ = False
-
-shuntingYard [] ops expr        = result 
-    where
-        [result]                = snd $ collapseWhile (const True) ops expr
-
-shuntingYard x y z = error (show (x, y, z))
-
+        [result] = snd $ collapseWhile (const True) ops expr
 
 collapseWhile :: (Token -> Bool) -> [Token] -> [Expression] -> ([Token], [Expression])
 collapseWhile f (o:os) expr
     | f o                         = collapseWhile f os (makeExpr o expr)
     | otherwise                   = ((o:os), expr)
-collapseWhile f [] expr = ([], expr)
+collapseWhile f [] expr           = ([], expr)
 
 makeExpr :: Token -> [Expression] -> [Expression]
 makeExpr (TUnaryOp '+') (x:xs)    = x: xs
