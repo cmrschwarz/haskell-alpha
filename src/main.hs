eac5a01e1b7977446e5be2f20c82b4d9ac611c30
Jakob Löw <jakob@m4gnus.de>
2019-04-12 17:14:37 +0200

    let main.hs detect whether the input is an equation and solve it as an equation instead of an expression

--- src/main.hs
+++ src/main.hs
@@ -4,11 +4,36 @@ import Solver.Backtracker
 
 import System.Environment
 import Data.List
+import Data.Maybe
+
+import Debug.Trace
+
+handleSolution solution = do
+    let solution' = map head $ group $ reverse solution
+    putStrLn $ intercalate "\n" $ map show solution'
+
+handleExpression str = do
+    let expr        = parseExpression $ str
+        solution    = solveExpression 6 expr
+
+    handleSolution solution
+
+handleEquation str = do
+    let (left, right)   = case elemIndices '=' str of
+            [pos]       -> splitAt pos str
+            _           -> error "Input contains more than one ="
+        leftExpr        = parseExpression left
+        rightExpr       = parseExpression $ tail right
+        equation        = Equation leftExpr rightExpr
+        solution        = solveEquation "x" 6 equation
+
+    handleSolution solution
 
 main = do
     args <- getArgs
-    let expr        = parseExpression $ intercalate " " args
-        solution    = solveExpression 5 expr
-        solution'   = map head $ group $ reverse solution
+    let str = intercalate " " args
+        isEquation  = isJust $ elemIndex '=' str
 
-    putStrLn $ intercalate "\n" $ map show solution'
+    if isEquation
+        then handleEquation str
+        else handleExpression str
